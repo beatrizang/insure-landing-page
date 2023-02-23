850d63ce988c5c04616680b4a520edb74f1c641d
Beatriz Garcia <beatrizgarciaang0@gmail.com>
2023-02-23 00:26:52 -0300

    Se cambia la manera de aparecer el menu responsive

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,14 +4,14 @@ let btnClose = document.getElementById('btn__close');
 let body = document.querySelector("body"); 
 
 document.getElementById('btn__menu').addEventListener('click',()=>{
-    nav.style.top = "60px";
+    nav.style.left = "0px";
     btnMenu.style.display = "none";
     btnClose.style.display = "flex";
     body.style.overflow = "hidden";
 });
 
 document.getElementById('btn__close').addEventListener('click', ()=>{
-    nav.style.top = "-100vh";
+    nav.style.left = "-800px";
     btnMenu.style.display = "flex";
     btnClose.style.display = "none";
     body.style.overflow = "visible";
